9589ebd2d89c81413464756c0eaf115b1c9134bb
Adelina1905 <temciucadelina14@gmail.com>
2026-03-31 18:56:43 +0300

    Added Cart and worked with useContext()

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 // import { Pizza } from "./Pizza";
 import PizzaOfTheDay from "./PizzaOdTheDay";
 import Order from "./Order";
-
+import Header from "./Header";
+import { CartContext } from "./contexts";
 const App = () => {
+  const cartHook = useState([]);
+
+
   return (
     <StrictMode>
+      <CartContext.Provider value = {cartHook}>
       <div>
-        <h1 className="logo">Padre Gino's</h1>
+        <Header />
         {/* <Pizza name="The Pepperoni Pizza" description="some dope pizza yo" image="/public/pizzas/pepperoni.webp" />
       <Pizza name="Americano Pizza" description="french fires and hotdogs" image="/public/pizzas/big_meat.webp" />
       <Pizza name="The Hawaiian" description="pineapple and ham" image="/public/pizzas/hawaiian.webp" />
@@ -16,7 +21,8 @@ const App = () => {
       <Pizza name="Baked Potato Pizza" description="wholy potato mash, wtf Minesota" image="/public/pizzas/ckn_pesto.webp" /> */}
         <Order />
         <PizzaOfTheDay/>
-      </div>
+      </div>  
+      </CartContext.Provider>
     </StrictMode>
 
   )
