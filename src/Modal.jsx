c9e67dd046194a58e646169b95c487affb4b1b25
Adelina1905 <temciucadelina14@gmail.com>
2026-08-13 11:20:30 +0300

    Modals

---
+++ src/Modal.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+import {createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+    const elRef = useRef(null); //ref is a fronzen object, the only thing that you can modify it's current
+
+    if(!elRef.current) {
+        elRef.current = document.createElement("div");// because elRef its a variable we will change just it not the entire dom, by creating this div, we will point just to this div every single render
+    }
+
+    useEffect(() => {
+        const modalRoot =document.getElementById("modal");
+        modalRoot.appendChild(elRef.current)
+
+        // return "TODO"
+
+        return ()=>modalRoot.removeChild(elRef.current)
+    }, [])
+
+    return createPortal(<div>{children}</div>, elRef.current)
+}
+
+export default Modal;
